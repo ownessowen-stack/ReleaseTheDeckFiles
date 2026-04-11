83ccdb0339ae02066de0539c1f10084aa61218e0
real_jarvis <ownessowen@gmail.com>
2026-04-11 16:47:26 +0300

    Delete usr/bin/open-onboard.sh

--- usr/bin/open-onboard.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# try to toggle keyboard if nnboard is already running
-dbus-send --type=method_call --dest=org.onboard.Onboard /org/onboard/Onboard/Keyboard org.onboard.Onboard.Keyboard.ToggleVisible
-# if nnboard not running open it
-if [ $? -ne 0 ]; then
-onboard
-fi
